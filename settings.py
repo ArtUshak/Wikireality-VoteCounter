2e57aa884c271e12591c83c51e93343cc5f9d111
Artiom Khandamirov <t9max@yandex.ru>
2018-10-24 07:58:59 +0300

    Removed unused settings

--- settings.py
+++ settings.py
@@ -1,19 +1,4 @@
 # -*- coding: utf-8 -*-
 """Settings for bot."""
-import logging
-import datetime
-
-
-log_file = '../Wikireality-VoteCounter-data/log.log'
-
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)-15s %(message)s',
-                    handlers=[logging.FileHandler(log_file),
-                              logging.StreamHandler()])
 
 api_uc_limit = 50
-
-output_file = '../Wikireality-VoteCounter-data/output.txt'
-
-start_date = datetime.datetime(2018, 7, 12)
-end_date = datetime.datetime(2018, 10, 12)
