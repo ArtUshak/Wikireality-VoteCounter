a7c2506b5a352cabdd6b400cdb32b45fdd8efdbc
Artiom Khandamirov <t9max@yandex.ru>
2018-10-27 19:52:02 +0300

    Fixed counting redirects as new pages, added docs

--- settings.py
+++ settings.py
@@ -2,3 +2,7 @@
 """Settings for bot."""
 
 api_uc_limit = 50
+
+redirect_regex_text = r'^Перенаправление на \[\[.+\]\]$'
+
+api_url = 'http://wikireality.ru/w/api.php'
--- votecounter.py
+++ votecounter.py
@@ -3,12 +3,16 @@
 import copy
 import datetime
 import json
+import re
 
 import click
 import requests
 
 import settings
-# from exceptions import MediaWikiAPIError
+from exceptions import MediaWikiAPIError
+
+
+regex_redirect_comment = re.compile(settings.redirect_regex_text)
 
 
 def get_user_contributions_count(user, start_date, end_date, namespace):
@@ -20,35 +24,45 @@ def get_user_contributions_count(user, start_date, end_date, namespace):
     """
     user_contributions_count = 0
     user_pages_count = 0
+    params = {
+        'action': 'query',
+        'list': 'usercontribs',
+        'uclimit': settings.api_uc_limit,
+        'ucnamespace': namespace,
+        'ucuser': user.replace(' ', '_'),
+        'ucdir': 'newer',
+        'format': 'json',
+    }
+    if start_date is not None:
+        params['ucstart'] = int(start_date.timestamp())
+    if end_date is not None:
+        params['ucend'] = int(end_date.timestamp())
+    last_continue = {}
 
     while True:
-        params = {
-            'action': 'query',
-            'list': 'usercontribs',
-            'uclimit': settings.api_uc_limit,
-            'ucnamespace': namespace,
-            'ucuser': user.replace(' ', '_'),
-            'ucdir': 'newer',
-            'format': 'json',
-        }
-        if start_date is not None:
-            params['ucstart'] = int(start_date.timestamp())
-        if end_date is not None:
-            params['ucend'] = int(end_date.timestamp())
-        r = requests.get('http://wikireality.ru/w/api.php', params=params)
+        current_params = params.copy()
+        current_params.update(last_continue)
+        r = requests.get(settings.api_url,
+                         params=current_params)
         if r.status_code != 200:
-            # raise MediaWikiAPIError()
-            raise Exception()
+            raise MediaWikiAPIError(None)
+
         data = r.json()
+        if 'error' in data:
+            raise MediaWikiAPIError(data['error'])
         contribs = data['query']['usercontribs']
-        user_pages_count += len(list(filter(lambda edit: 'new' in edit,
-                                            contribs)))
+
+        user_pages_count += len(list(
+            filter((lambda edit:
+                    (('new' in edit)
+                     and (regex_redirect_comment.match(
+                         edit['comment']) is None))),
+                   contribs)))
         user_contributions_count += len(contribs)
+
         if 'query-continue' not in data:
             break
-        start_date = datetime.datetime.strptime(
-            data['query-continue']['usercontribs']['ucstart'],
-            '%Y-%m-%dT%H:%M:%SZ')
+        last_continue = data['query-continue']['usercontribs']
 
     return (user_contributions_count, user_pages_count)
 
@@ -97,6 +111,7 @@ def run(user_list_file, namespacefile, start, end, output_format):
 
     users_data = dict()
     for user in users:
+        click.echo('Processing user {}...'.format(user))
         user_vote_power = 0.0
         user_new_pages = 0
         user_data = dict()
@@ -118,10 +133,10 @@ def run(user_list_file, namespacefile, start, end, output_format):
 
     if format == 'txt':
         for user in users_data:
-            click.echo('User {}\n'.format(user))
+            click.echo('User {}'.format(user))
             for key in user_data:
-                click.echo('{}: {}\n'.format(key, user_data[key]))
-            click.echo('\n')
+                click.echo('{}: {}'.format(key, user_data[key]))
+            click.echo('')
     elif output_format == 'json':
         click.echo(json.dumps(users_data))
     elif output_format == 'mediawiki':
